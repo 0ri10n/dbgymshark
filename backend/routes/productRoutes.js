629124d247151fd8437763ba65f1d986b0793722
0ri10n <kevineduardop614@gmail.com>
2026-01-31 19:05:18 -0600

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,5 @@ router.post('/', operatorCRUD.crearProducto);
 router.get('/:handle', operatorCRUD.obtenerProductoPorHandle);
 router.put('/:id', operatorCRUD.actualizarProducto);
 router.delete('/:id', operatorCRUD.eliminarProducto);
-router.post('/ventas', productController.registrarVenta);
-
+router.post('/ventas', operatorCRUD.registrarVenta);
 module.exports = router;
