0f2d76e1a3fac42d077dae44369b4dd30832e700
Kevin Patiño <al03009017@tecmilenio.mx>
2026-02-19 06:59:52 -0600

    Prueba link mejorado

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// --- SERVIR ARCHIVOS ESTÁTICOS ---
-// Usamos path.join para evitar errores de barras diagonales entre Windows y Linux
-app.use(express.static(path.join(__dirname, '../frontend')));
-
 // 4. Importar Rutas
 const authRoutes = require('./routes/authRoutes'); 
 const productRoutes = require('./routes/productRoutes');
@@ -32,20 +28,22 @@ app.use('/api/admin', adminRoutes);
 
 // --- RUTAS DEL FRONTEND ---
 
-// Ruta raíz: Envía al login
+// 1. Ruta raíz: Primero definimos que al entrar a "/" cargue el login
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/login/login.html'));
 });
 
-// Ruta de la tienda (Cliente)
+// 2. Otras rutas específicas
 app.get('/store', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/clientview/client.html'));
 });
 
-// Ruta del Panel de Administración
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/adminview/admin.html'));
 });
+
+// 3. ARCHIVOS ESTÁTICOS: Al final para que no interfiera con las rutas anteriores
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // --- MANEJO DE ERRORES 404 (Opcional pero recomendado) ---
 // Si alguien busca una ruta que no existe, le mandamos al login o una página 404
