ddb4dc144ddb18e1501af8eadf64e7889b278a0a
diegoabdi179 <diegoabdi179@gmail.com>
2026-02-18 18:18:54 -0600

    Cambio de base de datos

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: 'DB',
+            dbName: 'DBGymshark',
             socketTimeoutMS: 45000,
             serverSelectionTimeoutMS: 10000,
             retryWrites: true,
