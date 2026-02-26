f961431adf07990f7f90d4446a76ece2b0650ab6
Kevin Patiño <al03009017@tecmilenio.mx>
2026-02-26 07:14:48 -0600

    Mi link

--- frontend-react/src/pages/AdminPanel.jsx
+++ frontend-react/src/pages/AdminPanel.jsx
@@ -30,7 +30,7 @@ const AdminPanel = () => {
     const [formDataUsuario, setFormDataUsuario] = useState({
         nombre: '', apellido: '', email: '', rol: 'cliente', password: '', direccion: ''
     });
-    const baseURL = import.meta.env.VITE_API_URL || 'https://dbgymshark.onrender.com';
+    const baseURL = import.meta.env.VITE_API_URL || 'https://dbgymshark.onrender.com/api';
     const cargarProductos = async () => {
         setCargando(true);
         try {
