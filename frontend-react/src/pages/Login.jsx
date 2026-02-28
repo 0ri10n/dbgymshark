984fc3a6a6f9d5d75f76628c971a1e183bf7f672
Isos <al07073224@gmail.com>
2026-02-27 18:46:33 -0600

    fondo de login

--- frontend-react/src/pages/Login.jsx
+++ frontend-react/src/pages/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
                 <Beams
                     beamWidth={3}
                     beamHeight={30}
-                    beamNumber={10}
+                    beamNumber={20}
                     lightColor="#ffffff"
                     speed={2}
                     noiseIntensity={1.75}
